b1b032f05ccc1c318f77f3584aefab6831224bc1
Bachtiyar Muclis Arief <66326559+bachtiyararief@users.noreply.github.com>
2023-11-18 17:20:59 +0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -9,10 +9,10 @@ st.set_page_config(
 @st.cache_resource
 def header():
     _, row1, _ = st.columns([0.1, 8, 0.1])
-    row1.title('Penyelesaian VRPTW dengan Algoritma Flower Pollination')
-    row1.subheader('Ismi Yayuk')
+    row1.markdown('<h1>Penyelesaian VRPTW dengan Algoritma Flower Pollination</h1>', unsafe_allow_html = True)
+    row1.markdown('<h3>Ismi Yayuk</h3>', unsafe_allow_html = True)
 
-    row1.markdown('<h1>🚗 Latar Belakang</h1>', unsafe_allow_html = True)
+    row1.markdown('<h4>🚗 Latar Belakang</h4>', unsafe_allow_html = True)
     row1.markdown('''
         Vehicle Routing Problem with Time Windows (VRPTW) merupakan suatu permasalahan penentuan rute kendaraan
         yang digunakan untuk melayani pelanggan yang melibatkan lebih dari satu kendaraan dengan batasan waktu,
@@ -21,5 +21,18 @@ def header():
         unsafe_allow_html = True
     )
 
+def tampilkan_data(data = None):
+    row1.markdown('<h4>📊 Data Digunakan</h4>', unsafe_allow_html = True)
+    row1.markdown('''
+        Data yang digunakan terdiri dari 25, 50, dan 100 pelanggan yang digunakan
+        untuk perhitungan pada program. Data diperoleh dari 
+        <i>http://neo.lcc.uma.es/vrp/vrp-instances/capacitated-vrp-with-time-windows-instances/</i>. 
+        Data yang diolah terdiri dari koordinat posisi customer, permintaan tiap customer termasuk
+        juga Time Windows dan Waktu Pelayanan.
+        ''',    
+        unsafe_allow_html = True
+    )
+
 if __name__ == '__main__':
     header()
+    tampilkan_data()
