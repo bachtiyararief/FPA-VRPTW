aabcc2a1f85d7c8ad8383bdc8ca850b991191567
Bachtiyar Muclis Arief <66326559+bachtiyararief@users.noreply.github.com>
2023-11-18 17:16:28 +0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -12,5 +12,14 @@ def header():
     row1.title('Penyelesaian VRPTW dengan Algoritma Flower Pollination')
     row1.subheader('Ismi Yayuk')
 
+    row1.markdown('<h1>🚗 Latar Belakang</h1>', unsafe_allow_html = True)
+    row1.markdown('''
+        Vehicle Routing Problem with Time Windows (VRPTW) merupakan suatu permasalahan penentuan rute kendaraan
+        yang digunakan untuk melayani pelanggan yang melibatkan lebih dari satu kendaraan dengan batasan waktu,
+        sehingga diperoleh rute dengan jarak minimum tanpa melanggar kendala kapasitas muatan kendaraan dan rentang waktu.
+        ''',
+        unsafe_allow_html = True
+    )
+
 if __name__ == '__main__':
     header()
