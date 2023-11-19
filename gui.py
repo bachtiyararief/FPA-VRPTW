0596faebadd35f689da9d5beb255ad9278c2f2f8
Bachtiyar Muclis Arief <66326559+bachtiyararief@users.noreply.github.com>
2023-11-19 19:41:40 +0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -221,10 +221,10 @@ if __name__ == '__main__':
         hasil_perhitungan(hasil = hasil_terbaik)
         
         _, row6, _ = st.columns([0.1, 8, 0.1])
-        row6.markdown(f'{"-".join(str(num) for num in permutasi_terbaik.loc[0].tolist())}')
+        row6.markdown(f'Rute Terbaik : {"-".join(str(num) for num in permutasi_terbaik.loc[0].tolist())}')
         
         for i in range(len(jarak_potong)):
-            row6.markdown(f'{"-".join(str(num) for num in rute_potong)}\t{jarak_potong[i]}')        
+            row6.markdown(f'Rute-{i+1} \t{"-".join(str(num) for num in rute_potong[i])}\tJarak = {jarak_potong[i]}')        
         
         row6.markdown(f'Total Jarak : {hasil_terbaik[-1]}')
     
