2a6e3f6a8fe0ec50bdb085d9e2419b3105bb1880
Bachtiyar Muclis Arief <66326559+bachtiyararief@users.noreply.github.com>
2023-11-19 21:27:19 +0700

    Update plot_grafik.py

--- plot_grafik.py
+++ plot_grafik.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import plotly.graph_objects as go
 
 def plot_pergerakan_fungsi_tujuan(y):
     x = range(1, len(y)+1, 1)
@@ -36,4 +37,59 @@ def plot_pergerakan_fungsi_tujuan(y):
         dtick = 1
     )
 
+    return(fig)
+
+def plot_rute_vrptw(data_coord, rute):
+    nodes = dict()
+    for key, value in data[['Coord. X', 'Coord. Y']].to_dict('index').items():
+        if(key == 0):
+            nodes.update({'Depot' : (value.get('Coord. X'), value.get('Coord. Y'))})
+        else:
+            nodes.update({f'Customer {key}' : (value.get('Coord. X'), value.get('Coord. Y'))})
+    
+    routes = list()
+    for i in rute:
+        r = list(map(lambda x: 'Depot' if x == 0 else f'Customer {x}', i))
+        routes.append(r)
+    
+    fig = go.Figure()
+    for node, (x, y) in nodes.items():
+        fig.add_trace(
+            go.Scatter(
+                x = [x], 
+                y = [y], 
+                mode = 'markers', 
+                text = [node], 
+                name = node
+            )
+        )
+    
+    i = 0
+    for route in routes:
+        x = [nodes[node][0] for node in route]
+        y = [nodes[node][1] for node in route]
+        x.append(x[0])
+        y.append(y[0])
+        fig.add_trace(
+            go.Scatter(
+                x = x, 
+                y = y, 
+                mode = 'lines', 
+                name = 'Route'
+            )
+        )
+        i += 1
+    
+    # Update layout
+    fig.update_layout(
+        title = 'Rute Terbaik Vehicle Routing Problem with Time Windows <sup>Pada Hasil Perhitungan FPA</sup>',
+        xaxis = dict(title = 'X'),
+        yaxis = dict(title = 'Y'),
+        height = 650,
+        width = 1200,
+        plot_bgcolor = 'rgba(0, 0, 0, 0)',
+        showlegend = False,
+        hovermode = 'closest'
+    )
+    
     return(fig)
