852c8fbf49a51e8872edf6fa0526c3b071e2faac
Bachtiyar Muclis Arief <66326559+bachtiyararief@users.noreply.github.com>
2023-11-19 21:39:43 +0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -238,7 +238,6 @@ if __name__ == '__main__':
             hide_index = True
         )
 
-        no, ro = plot_rute_vrptw(data_coord = data_vrptw, rute = rute_terbaik)
-        row6.markdown(f'{no}<br><br>{ro}', unsafe_allow_html = True)
-        #row6.plotly_chart(fig_rute, use_container_width = True)
+        fig_vrptw = plot_rute_vrptw(data_coord = data_vrptw, rute = rute_potong)
+        row6.plotly_chart(fig_vrptw, use_container_width = True)
         
